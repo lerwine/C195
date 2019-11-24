69668a81a130e068d0df89f002b853fb3fa1723f
lerwine <lenny@erwinefamily.net>
2019-11-24 03:33:04 -0500

    Added initial language support.

--- Scheduler/src/controller/LoginScreenController.java
+++ Scheduler/src/controller/LoginScreenController.java
@@ -6,15 +6,22 @@
 package controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javax.persistence.EntityManager;
@@ -27,7 +34,9 @@ import utils.InvalidOperationException;
  * @author webmaster
  */
 public class LoginScreenController implements Initializable {
-
+    @FXML
+    private ComboBox languageComboBox;
+    
     @FXML
     private Label userNameLabel;
 
@@ -38,13 +47,46 @@ public class LoginScreenController implements Initializable {
     private Label passwordLabel;
 
     @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
     private TextField passwordTextField;
 
+    private ObservableList<String> languages;
+    
+    private HashMap<String, Locale> locales;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        locales = new HashMap<>();
+        languages = FXCollections.observableArrayList();
+        Stream.of(new Locale("en"), new Locale("es"), new Locale("de")).forEach((l) -> {
+            String key = l.getDisplayName(l);
+            locales.put(key, l);
+            languages.add(key);
+        });
+        languageComboBox.setItems(languages);
+        String n = languages.get(0);
+        languageComboBox.setValue(n);
+        setFromResource(ResourceBundle.getBundle("Messages", locales.get(n)));
+    }
+    
+    void setFromResource(ResourceBundle rb) {
+        userNameLabel.setText(rb.getString("userName") + ":");
+        passwordLabel.setText(rb.getString("password") + ":");
+        loginButton.setText(rb.getString("login"));
+        exitButton.setText(rb.getString("exit"));
+    }
+    
+    @FXML
+    void languageChanged(ActionEvent event) {
+        setFromResource(ResourceBundle.getBundle("Messages", locales.get((String)languageComboBox.getValue())));
     }
     
     @FXML
