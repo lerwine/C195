cc2624c8388f424c336d79d67c31ea75ff22f033
lerwine <lenny@erwinefamily.net>
2019-12-08 21:54:11 -0500

    Got field initialization working

--- Scheduler/src/controller/ControllerBase.java
+++ Scheduler/src/controller/ControllerBase.java
@@ -3,6 +3,7 @@ package controller;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,25 +27,51 @@ public abstract class ControllerBase implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Class<?> c = getClass();
-        Class<ResourceName> r = ResourceName.class;
-        if (r.isAnnotationPresent(r)) {
-            String resourceName = ((ResourceName)r.getAnnotation(r)).value();
+        Class<?> currentClass = getClass();
+        Class<ResourceName> resourceNameClass = ResourceName.class;
+        if (currentClass.isAnnotationPresent(resourceNameClass)) {
+            String resourceName = ((ResourceName)currentClass.getAnnotation(resourceNameClass)).value();
             if (resourceName == null || resourceName.trim().isEmpty())
                 Logger.getLogger(getClass().getName()).log(Level.WARNING, "Resource name annotation is empty");
             else {
-                Class<ResourceKey> k = ResourceKey.class;
-                ResourceBundle currentRb = ResourceBundle.getBundle(resourceName, App.getCurrentLocale());
-                Arrays.stream(c.getFields()).forEach((Field f) -> {
-                    if (f.isAnnotationPresent(k)) {
-                        String key = ((ResourceKey)f.getAnnotation(k)).value();
-                        if (currentRb.containsKey(key)) {
-                            try {
-                                Object value = f.get(this);
-                                if (value == null)
-                                    Logger.getLogger(getClass().getName()).log(Level.WARNING, "Field \"%s\" is null", f.getName());
+                Class<ResourceKey> resourceKeyClass = ResourceKey.class;
+                final ResourceBundle defaultBundle = ResourceBundle.getBundle(resourceName, App.getCurrentLocale());
+                
+                String bc = ControllerBase.class.getName();
+                final HashMap<String, ResourceBundle> loadedBundles = new HashMap<>();
+                loadedBundles.put(resourceName, defaultBundle);
+                do {
+                    Arrays.stream(currentClass.getDeclaredFields()).forEach((Field field) -> {
+                        if (java.lang.reflect.Modifier.isStatic(field.getModifiers()) || !field.isAnnotationPresent(resourceKeyClass))
+                            return;
+                        String key = ((ResourceKey)field.getAnnotation(resourceKeyClass)).value();
+                        ResourceBundle currentBundle = defaultBundle;
+                        if (field.isAnnotationPresent(resourceNameClass)) {
+                            String rbName = ((ResourceName)getClass().getAnnotation(resourceNameClass)).value();
+                            if (rbName != null && !rbName.trim().isEmpty()) {
+                                if (loadedBundles.containsKey(rbName))
+                                    currentBundle = loadedBundles.get(rbName);
                                 else {
-                                    String text = currentRb.getString(key);
+                                    currentBundle = ResourceBundle.getBundle(rbName, App.getCurrentLocale());
+                                    loadedBundles.put(rbName, currentBundle);
+                                }
+                            }
+                        }
+                        if (currentBundle.containsKey(key)) {
+                            try {
+                                boolean wasAccessible = field.isAccessible();
+                                if (!wasAccessible)
+                                    field.setAccessible(true);
+                                Object value;
+                                try { value = field.get(this); }
+                                finally {
+                                    if (!wasAccessible)
+                                        field.setAccessible(false);
+                                }
+                                if (value == null)
+                                    Logger.getLogger(getClass().getName()).log(Level.WARNING, "Field \"%s\" is null", field.getName());
+                                else {
+                                    String text = currentBundle.getString(key);
                                     if (value instanceof Labeled)
                                         ((Labeled)value).setText(text);
                                     else if (value instanceof MenuItem)
@@ -53,16 +80,16 @@ public abstract class ControllerBase implements Initializable {
                                         ((TableColumnBase<?,?>)value).setText(text);
                                     else
                                         Logger.getLogger(getClass().getName()).log(Level.WARNING, "Field \"%s\" value type %s is not supported",
-                                            new Object[] { f.getName(), value.getClass().getName() });
+                                            new Object[] { field.getName(), value.getClass().getName() });
                                 }
                             } catch (IllegalAccessException ex) {
                                 Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
                             }
-                            
                         } else
                             Logger.getLogger(getClass().getName()).log(Level.WARNING, "Cannot find resource key \"%s\"", key);
-                    }
-                });
+                    });
+                    currentClass = currentClass.getSuperclass();
+                } while (!bc.equals(currentClass.getName()));
             }
         } else
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, "No resource name annotation present");
