36d4eecda2dbea8af335d0f5e11bb27edea03081
Leonard T. Erwine <lenny@erwinefamily.net>
2020-04-21 09:11:14 -0400

    Removed unnecessary backup folder

--- Backup/model/IUserDataRow.java
+++
@@ -1,9 +0,0 @@
-package scheduler.model;
-
-/**
- *
- * @author Leonard T. Erwine (Student ID 356334) <lerwine@wgu.edu>
- */
-public interface IUserDataRow extends IDataRow {
-    String getUserName();
-}
